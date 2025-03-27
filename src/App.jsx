480ca86bddb596e93346cd93e9568aa3f1894c4c
Arthour98 <artourbouloudis@gmail.com>
2025-03-28 01:01:55 +0200

    somehow i installed tailwind :D

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 import './App.css'
 import TransactionGrid from './components/GridOfTransactions';
+import './index.css'
 
 function App() {
 
 
   return (
 <>
+    <div className='flex bg-amber-300  border'>
     <TransactionGrid/>
+    </div>
 </>
   )
 }
